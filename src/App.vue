48fbfaa299b30ea3e0f8c5ce1b1e44f00c6dbf40
sumin04 <gimsumin259@gmail.com>
2022-08-08 21:18:15 +0900

    초기설정

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <abc />
+  </div>
+</template>
+
+<script>
+import abc from './components/HelloWorld.vue'
+
+export default {
+  components: {
+    abc,
+  }
+}
+</script>
+
+<style>
+  
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>이것은 메뉴입니다.</h1>
+    <ul>
+      <li>첫화면</li>
+      <li>소개</li>
+      <li>게시판</li>
+      <li>문의</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  prompt : {
+    abc : String,
+  }
+}
+</script>
+
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+    text-align: center;
+  }
+  div{
+    margin: 20px;
+  }
+  ul > li{
+    float: left;
+    margin: 30px 30px 30px 30px;
+  }
+</style>
